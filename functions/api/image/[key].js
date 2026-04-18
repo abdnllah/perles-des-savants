51815508ea6733192abda74f83ac927c0947d49b
abdnllah <abdnllah@proton.me>
2026-04-18 20:50:53 +0000

    Update [key].js

--- functions/api/image/[key].js
+++ functions/api/image/[key].js
@@ -1,16 +1,19 @@
 export async function onRequest(context) {
   const { env, params } = context;
-  const key = params.key; // Récupère le nom de l'image dans l'URL
+  const key = params.key;
 
+  // 1. Récupérer l'image dans le bucket R2
   const object = await env.BUCKET.get(key);
 
   if (!object) {
-    return new Response("Image introuvable", { status: 404 });
+    return new Response("Image non trouvée", { status: 404 });
   }
 
+  // 2. Préparer les entêtes (type de fichier, etc.)
   const headers = new Headers();
   object.writeHttpMetadata(headers);
   headers.set("etag", object.httpEtag);
 
+  // 3. Renvoyer l'image au navigateur
   return new Response(object.body, { headers });
 }
